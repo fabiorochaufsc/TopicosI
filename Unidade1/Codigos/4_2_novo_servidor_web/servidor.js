1a5b7c8288c4ee266783f8afb7d65fe9506177b2
fabio.rocha <fabio.rocha@gmail.com>
2020-09-03 10:41:55 -0300

    atualiza

---
+++ Unidade1/Codigos/4_2_novo_servidor_web/servidor.js
@@ -0,0 +1,53 @@
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+var valor=0;
+app.get('/', function(req, resp) {
+	
+    console.log('Dentro da primeira parte');
+
+    resp.send('teste');  
+    return resp.end();
+});
+
+app.get('/leiaSensor/:id', function(req, resp) {
+   console.log('Retorna o valor do sensor'+req.params.id);
+   resp.send('valor do sensor'+valor);
+   valor=valor+1;
+   return resp.end();
+});
+
+///  http://localhost:3000/sensores?temperatura=123
+app.get('/sensores', function(req, resp) {
+
+    //console.log(req);
+    var temperatura = req.query.temperatura;
+    var humidade = req.query.humidade;
+    var id = req.query.id;
+
+    if (id) console.log('identificacao=', id);
+    if (temperatura) console.log('temperatura=', temperatura)
+    if (humidade) console.log('humidade=', humidade)
+
+	resp.send("fim da mensagem\n");
+    return resp.end();
+});
+
+
+
+app.get(/^(.+)$/, function(req, res) {
+    try {
+        res.write("A pagina que vc busca nao existe")
+        res.end();
+    } catch (e) {
+        res.end();
+    }
+})
+
+app.listen(3000, function() {
+    console.log("servidor no ar");
+});
